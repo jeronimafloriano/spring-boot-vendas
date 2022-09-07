58c8d39f30794ccfd0f52439de8715888204b71e
jeronima <jeronima.floriano@totvs.com.br>
2022-09-07 19:23:27 -0300

    Criando o modelo

--- vendas/src/main/java/jeronimafloriano/com/github/VendasApplication.java
+++ vendas/src/main/java/jeronimafloriano/com/github/VendasApplication.java
@@ -1,7 +1,12 @@
 package jeronimafloriano.com.github;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VendasApplication {
